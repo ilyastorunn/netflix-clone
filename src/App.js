e88806d0bfdf7a57e5515184aa08ec4aae02a147
İlyas Torun <ilyastorun@Ilyas-MacBook-Air.local>
2023-12-28 02:11:29 +0300

    added API Key and minor Front-End changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-// import './App.css';
+import "./App.css";
 import HomeScreen from './HomeScreen';
+import Banner from './Banner';
+import requests from './Requests';
 
 function App() {
   return (
     <div className="App">
       <HomeScreen />
+      
     </div>
   );
 }
