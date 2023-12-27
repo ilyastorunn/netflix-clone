952cf89694d5dabd3a3e089c721cdb0760e7e472
İlyas Torun <ilyastorun@Ilyas-MacBook-Air.local>
2023-12-28 00:00:17 +0300

    first section of Netflix Clone

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDW_tMnpi0gHH0A7tU-_rJVUg1eV3jfC1U",
+    authDomain: "netflix-clone-a02f6.firebaseapp.com",
+    projectId: "netflix-clone-a02f6",
+    storageBucket: "netflix-clone-a02f6.appspot.com",
+    messagingSenderId: "1003001894669",
+    appId: "1:1003001894669:web:de84128816f57322080244"
+  };
