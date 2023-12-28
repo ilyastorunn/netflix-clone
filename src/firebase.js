7d2367c96543ab8085067ecd235439b462263e5c
İlyas Torun <ilyastorun@Ilyas-MacBook-Air.local>
2023-12-28 14:13:38 +0300

    deployed with Firebase and minor changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,12 @@
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDW_tMnpi0gHH0A7tU-_rJVUg1eV3jfC1U",
-    authDomain: "netflix-clone-a02f6.firebaseapp.com",
-    projectId: "netflix-clone-a02f6",
-    storageBucket: "netflix-clone-a02f6.appspot.com",
-    messagingSenderId: "1003001894669",
-    appId: "1:1003001894669:web:de84128816f57322080244"
-  };
+  apiKey: "AIzaSyDW_tMnpi0gHH0A7tU-_rJVUg1eV3jfC1U",
+  authDomain: "netflix-clone-a02f6.firebaseapp.com",
+  projectId: "netflix-clone-a02f6",
+  storageBucket: "netflix-clone-a02f6.appspot.com",
+  messagingSenderId: "1003001894669",
+  appId: "1:1003001894669:web:de84128816f57322080244",
+};
+
+const app = initializeApp(firebaseConfig);
